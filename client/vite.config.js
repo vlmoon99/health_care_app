956d18184d4e6abfb971858312e78dfbbc870bc4
Vlad <vlmoon@proton.me>
2025-09-22 12:45:59 +0000

    (feature): add pwa support into app

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,58 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'favicon.ico',
+        'apple-touch-icon-180x180.png',
+        'logo.svg'
+      ],
+      manifest: {
+        name: 'Food Analyzer',
+        short_name: 'Food Analyzer',
+        description: 'Food analyzer',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        orientation: 'portrait',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      },
+      devOptions: {
+        enabled: true
+      }
+    })
   ],
   server: {
     allowedHosts: ['99bbfae3062e.ngrok-free.app']
